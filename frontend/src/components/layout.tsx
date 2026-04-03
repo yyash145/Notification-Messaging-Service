03e69339f6fe7d3be29d4c832e12854aef8542c9
Yash Maheshwari <yyash145@Yashs-MacBook.local>
2026-04-03 19:21:52 +0530

    Remove Lint Errors

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -8,69 +8,12 @@ type Props = {
 
 const Layout: React.FC<Props> = ({ onLogout }) => {
   const navigate = useNavigate();
-  const [open, setOpen] = useState(false);
-  const [users, setUsers] = useState<any[]>([]);
   const userRole = localStorage.getItem("userRole");
-  const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 
   const handleLogoutClick = () => {
     onLogout();
     navigate("/login");
   };
-
-  const fetchUsers = async () => {
-    try {
-        const res = await fetch(`${BASE_URL}/users/getAllUsers`, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-        });
-
-        const data = await res.json();
-
-        // 🔥 handle all possible shapes
-        if (Array.isArray(data)) {
-            setUsers(data);
-        } else if (Array.isArray(data.data)) {
-            setUsers(data.data);
-        } else if (Array.isArray(data.users)) {
-            setUsers(data.users);
-        } else {
-            setUsers([]); // ✅ always array
-        }
-        
-      } catch (err) {
-          console.error(err);
-          setUsers([]); // fallback
-      }
-
-    };
-
-  const handleDeleteAllUsers = async () => {
-    try {
-      const confirmDelete = window.confirm(
-        "Are you sure you want to delete all users except yourself?"
-      );
-
-      if (!confirmDelete) return;
-
-      await fetch(`${BASE_URL}/users/removeAllUsers`, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-      });
-
-      alert("All users (except you) deleted successfully");
-    } catch (err) {
-      console.error(err);
-      alert("Error deleting users");
-    }
-    finally{
-      fetchUsers()
-    }
-  };
-
 
   return (
     <div className="page-container">
